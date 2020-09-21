504a71261bcb48d1afe64afe4335ab0184fc2103
Double0negative <soccerkid9094@gmail.com>
2020-09-20 23:08:49 -0700

    Init

---
+++ src/main/java/com/minehut/Template.java
@@ -0,0 +1,11 @@
+package com.minehut;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Template extends JavaPlugin{
+
+	@Override
+	public void onEnable() {
+		System.out.println("Enabled Template Plugin");
+	}
+}
